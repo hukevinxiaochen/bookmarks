cf1e687cb64f60f64c07ae62ce72ded8743069ed
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-12-26 10:36:49 -0500

    Use material-ui library.

--- public/view.js
+++ public/view.js
@@ -3,14 +3,33 @@
 /*
  * Front-end libraries we rely on.
  * - React
+ * - Material-UI
 */
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Provides onTouchTap prop
+import injectTapEventPlugin from "react-tap-event-plugin";
+injectTapEventPlugin();
+
+// Root component
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+// Included components
+import RaisedButton from "material-ui/RaisedButton";
+import ActionCached from "material-ui/svg-icons/action/cached";
+
+const App = () => (
+  <MuiThemeProvider>
+    <RaisedButton 
+      label="Sync" 
+      icon={<ActionCached />}
+    />
+  </MuiThemeProvider>
+);
 
 // Render
 ReactDOM.render((
-  <p>
-    <em>React components to come soon!</em>
-  </p>
-  ), document.getElementById('app'));
+  <App />
+    ), document.getElementById('app'));
 

55b0eb7f650486fbf715f88ffb2ed7caf876cefc
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-12-26 08:15:02 -0500

    Ignore bundled JavaScript

--- bookmarks.rb
+++ bookmarks.rb
@@ -2,15 +2,14 @@ require "sinatra/base"
 require 'liquid'
 
 class Bookmarks < Sinatra::Base
+
   get '/' do
     # execute a shell command
     convert_markdown_to_html = "pandoc data/bookmarks.markdown -o data/output.html"
     system(convert_markdown_to_html)
-    IO.read("data/output.html")
-  end
+    bookmarks = IO.read("data/output.html")
 
-  get '/react' do
-    liquid :index
+    liquid :index, :locals => { :bookmarks => bookmarks }
   end
 end
 

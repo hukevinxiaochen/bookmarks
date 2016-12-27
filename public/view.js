5fb6503553f12e20a9edb03f420af999a72093bf
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-12-27 05:19:57 -0500

    Use imports instead of require.
    Import statements don't give me the weird
    
    `React.createElement type should not be null, undefined.. When
    creating/rendering components`
    
    bug.

--- public/view.js
+++ public/view.js
@@ -1,24 +1,27 @@
-// index.js
+// view.js
 
 /*
  * Front-end libraries we rely on.
  * - React
  * - Material-UI
 */
-import React from "react";
-import ReactDOM from "react-dom";
 
-// Provides onTouchTap prop
+// React
+import React from                "react";
+import ReactDOM from             "react-dom";
+
+// Plugin for tap events
 import injectTapEventPlugin from "react-tap-event-plugin";
 injectTapEventPlugin();
 
 // Root component
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import MuiThemeProvider from     "material-ui/styles/MuiThemeProvider";
 
 // Included components
-import RaisedButton from "material-ui/RaisedButton";
-import ActionCached from "material-ui/svg-icons/action/cached";
+import RaisedButton from         "material-ui/RaisedButton";
+import ActionCached from         "material-ui/svg-icons/action/cached";
 
+// Define App
 const App = () => (
   <MuiThemeProvider>
     <RaisedButton 
@@ -28,8 +31,17 @@ const App = () => (
   </MuiThemeProvider>
 );
 
-// Render
-ReactDOM.render((
-  <App />
-    ), document.getElementById('app'));
+/* Render
+ *
+*/
 
+// Client-side
+ReactDOM.render( ( <App /> ), document.getElementById( 'app' ) );
+
+// Server side
+// syncButton = ReactDOMServer.renderToStaticMarkup(App);
+// console.log("Hello");
+
+// Export
+// module.exports = App;
+// We are top-level
